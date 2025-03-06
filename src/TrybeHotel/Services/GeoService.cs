0b02ba7f3d6992d06e2a5ddb5beec704cd3f2180
p4n1k0 <gabre.tarick@gmail.com>
2025-03-06 20:18:54 -0300

    endpoint GET /get/status

--- src/TrybeHotel/Services/GeoService.cs
+++ src/TrybeHotel/Services/GeoService.cs
@@ -6,18 +6,21 @@ namespace TrybeHotel.Services
 {
     public class GeoService : IGeoService
     {
-         private readonly HttpClient _client;
+        private readonly HttpClient _client;
         public GeoService(HttpClient client)
         {
             _client = client;
+            _client.BaseAddress = new Uri("https://nominatim.openstreetmap.org/");
         }
 
         // 11. Desenvolva o endpoint GET /geo/status
         public async Task<object> GetGeoStatus()
         {
-            throw new NotImplementedException();
+            var url = await _client.GetAsync("status.php?format=json");
+            var response = await url.Content.ReadFromJsonAsync<object>();
+            return response!;
         }
-        
+
         // 12. Desenvolva o endpoint GET /geo/address
         public async Task<GeoDtoResponse> GetGeoLocation(GeoDto geoDto)
         {
@@ -30,23 +33,25 @@ namespace TrybeHotel.Services
             throw new NotImplementedException();
         }
 
-       
 
-        public int CalculateDistance (string latitudeOrigin, string longitudeOrigin, string latitudeDestiny, string longitudeDestiny) {
-            double latOrigin = double.Parse(latitudeOrigin.Replace('.',','));
-            double lonOrigin = double.Parse(longitudeOrigin.Replace('.',','));
-            double latDestiny = double.Parse(latitudeDestiny.Replace('.',','));
-            double lonDestiny = double.Parse(longitudeDestiny.Replace('.',','));
+
+        public int CalculateDistance(string latitudeOrigin, string longitudeOrigin, string latitudeDestiny, string longitudeDestiny)
+        {
+            double latOrigin = double.Parse(latitudeOrigin.Replace('.', ','));
+            double lonOrigin = double.Parse(longitudeOrigin.Replace('.', ','));
+            double latDestiny = double.Parse(latitudeDestiny.Replace('.', ','));
+            double lonDestiny = double.Parse(longitudeDestiny.Replace('.', ','));
             double R = 6371;
             double dLat = radiano(latDestiny - latOrigin);
             double dLon = radiano(lonDestiny - lonOrigin);
-            double a = Math.Sin(dLat/2) * Math.Sin(dLat/2) + Math.Cos(radiano(latOrigin)) * Math.Cos(radiano(latDestiny)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
-            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1-a));
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Cos(radiano(latOrigin)) * Math.Cos(radiano(latDestiny)) * Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
             double distance = R * c;
-            return int.Parse(Math.Round(distance,0).ToString());
+            return int.Parse(Math.Round(distance, 0).ToString());
         }
 
-        public double radiano(double degree) {
+        public double radiano(double degree)
+        {
             return degree * Math.PI / 180;
         }
 
